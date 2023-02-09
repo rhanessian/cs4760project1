6d7700d5da4e4e9e2ac5fd350e6db13efb8b797c
rhanessian <80182115+rhanessian@users.noreply.github.com>
2023-02-08 23:30:18 -0600

    Add files via upload

--- oss.c
+++ oss.c
@@ -3,17 +3,24 @@
 //Project 1: Linux System Calls-Skeleton Multiple Processes
 
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 void print_usage (const char* argmt){
-	printf(stderr, "Usage: %s [-h] [-n proc] [-s simul] [-t iter]\n", \argmt);
-	printf(stderr, "	proc is the number of worker processes to launch\n");
-	printf(stderr, "	simul is the number of processes that can run simultaneously\n");
-	printf(stderr, "	iter is the number to be passed to the worker processes\n");
-	printf(stderr, "Default proc is 15, default simul is 5, default iter is 10.\n");
+	fprintf(stderr, "Usage: %s [-h] [-n proc] [-s simul] [-t iter]\n", argmt);
+	fprintf(stderr, "	proc is the number of worker processes to launch\n");
+	fprintf(stderr, "	simul is the number of processes that can run simultaneously\n");
+	fprintf(stderr, "	iter is the number to be passed to the worker processes\n");
+	fprintf(stderr, "Default proc is 15, default simul is 5, default iter is 10.\n");
 }
 
-int main (int argc, const char *argv[]){
+int main (int argc, char *argv[]){
 	pid_t workerpid = 0;
-	int i, j, opt;
+	int i, j;
+	
+	char opt;
+
 	int proc = 15, simul = 5, iter = 10;
 	
 	opterr = 0;
@@ -33,7 +40,7 @@ int main (int argc, const char *argv[]){
 				print_usage (argv[0]);
 				return (EXIT_SUCCESS);
 			default:
-				printf ("Invalid option %c\n", optopt);
+				printf ("Invalid option %c\n", opt);
 				print_usage (argv[0]);
 				return (EXIT_FAILURE);		}
 }
